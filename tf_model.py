7ef65aa344243d4012dbfd250cadca0956e6629a
lmcconnell1665 <56060247+lmcconnell1665@users.noreply.github.com>
2020-10-04 17:14:16 -0400

    added model code

--- tf_model.py
+++ tf_model.py
@@ -1,59 +1,95 @@
-!pip3 install --upgrade tensorflow
-#alternatively in the terminal: 
-#python3 -m pip install --upgrade tensorflow
+"""
+Tuning-NeuralNetwork
+10/4/2020
+Dunn, McGinnis, McConnell
+"""
+
 import tensorflow as tf
+import pandas as pd
+from sklearn import datasets, linear_model
+from sklearn.model_selection import train_test_split
+
 tf.__version__
 
-#Create some nonlinear toy data.
-import matplotlib.pyplot as plt
-import numpy as np
-ct = np.ones(20) 
-X1 = np.random.normal(size=20) #variable, 20 rows
-X2 = np.random.normal(size=20) #variable, 20 rows
-X = np.array(np.column_stack((X1,X2)))
-y = ct*2.2222 + X1*5.4675 + + X2*10.1115 - 3*X1**2
+#####################
+#### DATA IMPORT ####
+#####################
 
+# Import data
+pricing = pd.read_csv('pricing.csv', sep = ';')
+pricing = pricing[0:100]
 
-###Implementing a neural regression network with the functional tf.keras API
+# Pull on y variable
+y = pricing['gross_margin'] / pricing['adjusted_duration_seconds_sum']
 
-#Specify architecture
-inputs = tf.keras.layers.Input(shape=(X.shape[1],), name='input') #Note: shape is a tuple and does not includes records. For a two dimensional input dataset, use (Nbrvariables,). We would use the position after the comma, if it would be a 3-dimensional tensor (e.g., images). Note that (something,) does not create a second dimension. It is just Python's way of generating a tuple (which is required by the Input layer).
-hidden1 = tf.keras.layers.Dense(units=2, activation="sigmoid", name = 'hidden1')(inputs)
-hidden2 = tf.keras.layers.Dense(units=2, activation="sigmoid", name= 'hidden2')(hidden1)
-output = tf.keras.layers.Dense(units=1, activation = "linear", name= 'output')(hidden2)
+# Keep only relavant x variables
+X = pricing[['master_id',
+                   'unit_cost',
+                   'host_full_name_1_array',
+                   'show_brand_label_1_array',
+                   'show_type_array',
+                   'showing_start_date_time_min',
+                   'merch_department',
+                   'merch_class_name',
+                   'country_of_origin',
+                   'unit_offer_price']]
 
-#Create model 
-model = tf.keras.Model(inputs = inputs, outputs = output)
+# Create training and holdout samples
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+print (X_train.shape)
+print (y_train.shape)
+print (X_test.shape)
+print (y_test.shape)
 
-#Compile model
-model.compile(loss = 'mse', optimizer = tf.keras.optimizers.SGD(lr = 0.001))
+#####################
+#### TF FUNCTION ####
+#####################
 
-#Fit model
-model.fit(x=X,y=y, batch_size=1, epochs=10) #this can be run any number of times and it will start from the last version of the weights. To reset the weights, rerun the specification to trigger the random initialization.
+def tune_parameters(batch_size_entry = 1, hidden_node_entry = 2, activation_func_entry = 'relu'):
 
-#making a prediction (first record)
-yhat = model.predict(x=X[0:1])
-#or
-yhat = model.predict(x=X[[0]])
-#making a prediction (all records)
-yhat = model.predict(x=X)
+    #Specify architecture
+    inputs = tf.keras.layers.Input(shape=(X_train.shape[1],), name='input')
 
-#getting the loss (on for example a test set)
-model.evaluate(X,y)
+    hidden1 = tf.keras.layers.Dense(units=hidden_node_entry, activation=activation_func_entry, name='hidden1')(inputs)
+    hidden2 = tf.keras.layers.Dense(units=hidden_node_entry, activation=activation_func_entry, name='hidden2')(hidden1)
+    output = tf.keras.layers.Dense(units=1, activation="linear", name='output')(hidden2)
+
+    #Create model 
+    model = tf.keras.Model(inputs = inputs, outputs = output)
+
+    #Compile model
+    model.compile(loss = 'mse', optimizer = tf.keras.optimizers.SGD(lr = 0.001))
+
+    #Fit model
+    model.fit(x=X_train,y=y_train, batch_size=batch_size_entry, epochs=10)
+
+    #making a prediction (first record)
+    yhat = model.predict(x=X_test[0:1])
+
+    #making a prediction (all records)
+    yhat = model.predict(x=X_test)
+
+    #getting the loss (on for example a test set)
+    return model.evaluate(X_test,y_test)
+
+#####################
+###### TUNING #######
+#####################
+
+tune_parameters(batch_size_entry = 10,
+                hidden_node_entry = 1,
+                activation_func_entry = 'relu')
+
+# Parameters that need to be added to tune: number of hidden layers & optimization function
+
+#####################
+#### OLD CODE #######
+#####################
 
 #summarizing the model
-model.summary()
+# model.summary()
 
-#getting the weights
-model.layers
-hidden1weights = model.layers[1]
-hidden1weights.get_weights()
-
-#plotting the loss:
-history = model.fit(x=X,y=y, batch_size=1, epochs=10)
-import pandas as pd
-import matplotlib.pyplot as plt
-pd.DataFrame(history.history).plot(figsize=(8,5))
-
-#Computing the validation loss during training (other data, or all the training data)
-model.fit(x=X,y=y, batch_size=1, epochs=10, validation_data=(X,y))
+# #getting the weights
+# model.layers
+# hidden1weights = model.layers[1]
+# hidden1weights.get_weights()
